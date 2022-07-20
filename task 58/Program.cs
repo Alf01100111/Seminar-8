b8360abadbe3a51bcdb2bc4228803bce21ec2732
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-20 22:07:07 +0300

    task 58 done

--- task 58/Program.cs
+++ task 58/Program.cs
@@ -23,31 +23,34 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
-int[,] matrix1 = GetRndMatrix(2, 2, 0, 10);
-int[,] matrix2 = GetRndMatrix(2, 2, 0, 10);
+int[,] matrix1 = GetRndMatrix(3, 4, 0, 10);
+int[,] matrix2 = GetRndMatrix(4, 3, 0, 10);
 
 PrintMatrix(matrix1);
-System.Console.WriteLine("---------------");
+System.Console.WriteLine("------------------");
 PrintMatrix(matrix2);
 
 int[,] MultiplicMatrix(int[,] matrix1, int[,] matrix2)
 {
-int[,] resultMatrix = new int[matrix2.GetLength(0), matrix1.GetLength(1)];
-    int k = 0;
-    int n = 0;
+    int[,] resultMatrix = new int[matrix2.GetLength(1), matrix1.GetLength(0)];
+    
     for (int i = 0; i < matrix1.GetLength(0); i++)
-        {
-            for (int j = 0; j < matrix1.GetLength(1); j++)
+    {
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
             {
-                resultMatrix[k, n] += matrix1[i, j] * matrix2[j, i];
+                System.Console.WriteLine("Умножение матриц не возможно!");break;
             }
-            n++;          
-        }
-        k++;
-     return resultMatrix;
+        for (int k = 0; k < matrix2.GetLength(1); k++)
+        {
+            for (int m = 0; m < matrix2.GetLength(0); m++)
+            {
+                resultMatrix[i, k] += matrix1[i, m] * matrix2[m, k]; 
+            }           
+        }        
+    }
+    return resultMatrix;
 }
 
 System.Console.WriteLine("------result------");
-int[,] resmatr = new int[2,2];
-resmatr = MultiplicMatrix(matrix1,matrix2);
-PrintMatrix(resmatr);
+
+PrintMatrix(MultiplicMatrix(matrix1, matrix2));
